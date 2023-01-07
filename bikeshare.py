dee372d18306fab2074b49bf25892e05b95f70bf
Jonas Nolte <jonas.nolte@riverty.com>
2023-01-07 09:36:17 +0100

    Removed HINT from the initial code template

--- bikeshare.py
+++ bikeshare.py
@@ -23,7 +23,7 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
-    # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+    # get user input for city (chicago, new york city, washington).
 
     while True:
         city = input("\nFor which city would you like to explore bikeshare data? \n\
@@ -106,7 +106,6 @@ def time_stats(df):
     # display the most common start hour
     top_start_hour = df['hour'].mode()[0]
     print("The most frequent start hour is: {}".format(top_start_hour))
-
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
